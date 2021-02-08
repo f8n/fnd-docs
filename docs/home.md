decfe3032c2e9e14434c9a8595837a20d2a2eb57
Paul Cowgill <pauldavidcowgill@gmail.com>
2021-02-08 16:14:11 -0600

    Configure FND Docs site

---
+++ docs/home.md
@@ -0,0 +1,35 @@
+---
+id: home
+title: Docs
+sidebar_label: Docs
+slug: /
+---
+
+---
+
+## Subgraphs
+
+[Mainnet](https://thegraph.com/explorer/subgraph/f8n/f8n-mainnet)
+
+[Goerli](https://thegraph.com/explorer/subgraph/f8n/f8n-goerli)
+
+---
+
+## Contracts
+
+```json
+{
+  "foundation": {
+    "address": "0x67Df244584b67E8C51B10aD610aAfFa9a402FdB6",
+    "implementation": "0xA428BC0b619Af0bbe7AF05fE5a3eC87A2f8ebA77"
+  },
+  "nftMarket": {
+    "address": "0xcDA72070E455bb31C7690a170224Ce43623d0B6f",
+    "implementation": "0x01b74373e98D4b511Aaaa66DD550bC5Ed980D739"
+  },
+  "nft721": {
+    "address": "0x3B3ee1931Dc30C1957379FAc9aba94D1C48a5405",
+    "implementation": "0xaCCbd3fde9d7f6e899B85003120A460d78b0707A"
+  }
+}
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Foundation Docs',
+  tagline: 'Foundation is building a new creative economy on top of the Ethereum network. Our infrastructure is open, permissionless and transparent.',
+  url: 'https://fnd.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'img/fnd.svg',
+  organizationName: 'f8n', // Your GitHub org name.
+  projectName: 'f8n-contracts', // Your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Foundation Logo',
+        src: 'img/fnd.svg',
       },
       items: [
         {
@@ -22,44 +21,38 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/f8n',
           label: 'GitHub',
           position: 'right',
         },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Foundation',
           items: [
             {
-              label: 'Style Guide',
+              label: 'Docs',
               to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
             },
           ],
         },
         {
-          title: 'Community',
+          title: 'Connect',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Twitter',
+              href: 'https://twitter.com/withFND',
             },
             {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              href: 'https://discord.foundation.app',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: 'Instagram',
+              href: 'https://www.instagram.com/withfoundation/',
             },
           ],
         },
@@ -68,16 +61,16 @@ module.exports = {
           items: [
             {
               label: 'Blog',
-              to: 'blog',
+              to: 'https://foundation.app/blog',
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/f8n',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Foundation Labs, Inc.`,
     },
   },
   presets: [
@@ -88,13 +81,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/f8n/f8n-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
